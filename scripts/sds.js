03830d80cb6ad7abd15bc5d3e4b56f7b95a89d66
Yosoy-Ed <eascdrb@gmail.com>
2024-07-26 19:10:49 -0500

    Fixing issue due to code reuse #6

--- scripts/sds.js
+++ scripts/sds.js
@@ -380,7 +380,7 @@ Hooks.on('midi-qol.RollComplete', (workflow) => {
 
     let chatMessage = workflow.chatCard;
 
-    if (!game.settings.get('critic-message', 'disablemodule')) {
+    if (!game.settings.get('simple-dice-stats', 'disablemodule')) {
 
         detectroll(chatMessage);
     }
